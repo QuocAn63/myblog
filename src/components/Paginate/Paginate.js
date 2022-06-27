e55cd1d8c9f24f84f8dfb43c7a360ecd96ebffc6
caoan <caoan632002@gmail.com>
2022-06-27 18:05:25 +0700

    Update: Search page, Fix: Filter, Pagination

--- src/components/Paginate/Paginate.js
+++ src/components/Paginate/Paginate.js
@@ -1,6 +1,6 @@
 import { memo } from 'react'
 import PropTypes from 'prop-types'
-import { Link } from 'react-router-dom'
+import { useSearchParams } from 'react-router-dom'
 import styles from './Paginate.module.scss'
 import classNames from 'classnames/bind'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -8,10 +8,17 @@ import { faAngleLeft, faAngleRight, faEllipsis } from '@fortawesome/free-solid-s
 
 const cx = classNames.bind(styles)
 
-function Paginate({ currentPage, totalPage, className}) {
+function Paginate({ totalPage, className}) {
+    const [SearchParams, setSearchParams] = useSearchParams()
+    const currentPage = Number.parseInt(SearchParams.get('page')) || 1;
 
-    const pageNumbers = () => {
-        const Page = ({Number}) => <Link to={`?page=${Number}`} className={cx('page')}>{Number}</Link>
+    const handleClick = (number) => {
+        const currentParams = Object.fromEntries([...SearchParams])
+        setSearchParams({...currentParams, page: number})
+    }
+
+    const PageNumbers = () => {
+        const Page = ({Number}) => <span to={`?page=${Number}`} onClick={() => handleClick(Number)} className={cx('page')}>{Number}</span>
         const ActivePage = ({Number}) => <span className={cx('page', 'active')}>{Number}</span> 
         const DotPage = (key) => <span key={key} className={cx('page', 'disabled')}><FontAwesomeIcon icon={faEllipsis} /></span> 
         let array = []
@@ -19,16 +26,22 @@ function Paginate({ currentPage, totalPage, className}) {
             for(let i = 1; i <= totalPage; i++) {
                 array.push(i)
             }
-        } else {
+        } else {    
             if(currentPage >= 7) {
-                for(let i=1; i<=totalPage; i++) {
-                    if((currentPage - 3 >= 4) && (totalPage - (currentPage + 3) > 2) && ((i >= currentPage - 3 && i <= currentPage + 3) || (i < 3 || i > totalPage - 2))) {
+                if((currentPage - 3 >= 4) && (totalPage - (currentPage + 3) > 2)) {
+                    for(let i=1; i<=totalPage; i++) {
+                        if(((i >= currentPage - 3 && i <= currentPage + 3) || (i < 3 || i > totalPage - 2))) {
+                            array.push(i)
+                        }
+                    }
+                } else if(currentPage - 3 >= 4) {
+                    for(let i=4; i<=currentPage; i++) {
                         array.push(i)
                     }
-                }
+                } else 
                 array.splice(2, 0, <FontAwesomeIcon icon={faEllipsis} />)
                 array.splice(array.length - 2, 0, <FontAwesomeIcon icon={faEllipsis} />)
-            }
+            }   
             // Continue
         }
 
@@ -40,21 +53,20 @@ function Paginate({ currentPage, totalPage, className}) {
         })
 
 
-        array.unshift(<Link key={-1} to={`?page=${currentPage - 1}`} className={cx('page', currentPage === 1 ? 'disabled' : null)}>{}<FontAwesomeIcon icon={faAngleLeft}></FontAwesomeIcon></Link>)
-        array.push(<Link key={totalPage + 1} to={`?page=${currentPage + 1}`} className={cx('page', currentPage === totalPage ? 'disabled' : null)}>{}<FontAwesomeIcon icon={faAngleRight}></FontAwesomeIcon></Link>)
+        array.unshift(<div key={-1} onClick={() => handleClick(currentPage - 1)} className={cx('page', currentPage === 1 ? 'disabled' : null)}>{}<FontAwesomeIcon icon={faAngleLeft}></FontAwesomeIcon></div>)
+        array.push(<div key={totalPage + 1} onClick={() => handleClick(currentPage + 1)} className={cx('page', currentPage === totalPage ? 'disabled' : null)}>{}<FontAwesomeIcon icon={faAngleRight}></FontAwesomeIcon></div>)
         
         return array
     }
 
   return (
     <div className={cx('wrapper', className)}>
-        {pageNumbers()}
+        <PageNumbers />
     </div>
   )
 }
 
 Paginate.propTypes = {
-    currentPage: PropTypes.number.isRequired,
     totalPage: PropTypes.number.isRequired,
     className: PropTypes.string
 }
